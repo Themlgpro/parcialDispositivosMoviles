33d9450f73842d3ebc274773eaf3fb4fa73650a9
GironArcila <sebastian.giron@correounivalle.edu.co>
2018-05-08 20:05:45 -0500

    toolBar

--- app/src/main/java/com/parcial/appsmoviles/parcial/Login.java
+++ app/src/main/java/com/parcial/appsmoviles/parcial/Login.java
@@ -1,7 +1,9 @@
 package com.parcial.appsmoviles.parcial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void irMain(View g){
+        Intent ir = new Intent(Login.this,MainActivity.class);
+        ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TOP | ir.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(ir);
+
     }
 }
---
+++ app/src/main/java/com/parcial/appsmoviles/parcial/MainActivity.java
@@ -0,0 +1,26 @@
+package com.parcial.appsmoviles.parcial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar= (Toolbar)findViewById(R.id.Menu);
+        setSupportActionBar(toolbar);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+}
